3591143edf4e82fea54bb9315629f29f5f172007
prasad <hackerspider09@gmail.com>
2024-06-25 11:00:04 +0530

    shell sript changed

--- FloatingNotesApp/setup.sh
+++ FloatingNotesApp/setup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-SCRIPT_PATH="/usr/local/FloatingNotes"
+###########################################################
+# Author : Prasad Balaji Khatake
+# Date : 18/02/2024
+# Use : Use to activate Floating Sticky Notes
+#
+# Desc : Floating Sticky notes application 
 
-# Log file path
+#############################################################
+
+SCRIPT_PATH=$(dirname "$(realpath "$0")")
+
+sleep 2
+
+# Log flie path
 LOG_FILE="$SCRIPT_PATH/floatingnotes.log"
 JAR_FILE="$SCRIPT_PATH/FloatingNotes.jar"
 
 # Change directory to the location of the JAR file
 cd "$SCRIPT_PATH" || exit
 
-echo "$SCRIPT_PATH = $LOG_FILE = $JAR_FILE"
-
-# Run application and redirect stderr stdout to log file
+# run application and redirect stderr stdout in log file
 java -jar $JAR_FILE >> "$LOG_FILE" 2>&1 &
 
 echo "FloatingNotes application started."
